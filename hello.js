39000fac2bb0d757cfa3bd1133967832dcd722d4
Abigail Addae <abigail2addae@gmail.com>
2021-11-13 02:54:38 +0000

    practice writing greeting function

---
+++ hello.js
@@ -0,0 +1,15 @@
+//Example 1
+// const sayHello = function () {
+//   console.log("Hello, world");
+// }
+
+// sayHello();
+
+//Example 2
+const sayHello = function(name) {
+  console.log("Hello, " + name);
+}
+
+sayHello("Caliban");
+sayHello("Miranda");
+sayHello("Ferdinand");
