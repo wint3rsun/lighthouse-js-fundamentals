89bf118d32aaee7b67ace58907124b60769f7874
Abigail Addae <abigail2addae@gmail.com>
2021-11-13 15:09:29 +0000

    added more practice with js files

---
+++ arrays.js
@@ -0,0 +1,12 @@
+const director = "Steven Spielerg";
+const films = ["Jaws", "Raiders of the Lost Ark", "A.I."];
+
+console.log("My favourite " + director + " film is " + films[1]);
+
+console.log("films at index 0: " + films[0]);
+console.log("films at index 1: " + films[1]);
+console.log("films at index 2: " + films[2]);
+console.log("films at index 3: " + films[3]);
+
+const oddNumbers = [1, 3, 5, 7, 9];
+const randomStuff = [1.15, "bicycle", true];
---
+++ ice-cream.js
@@ -0,0 +1,18 @@
+// Do not modify these first two lines
+const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+console.log(iceCreamFlavours);
+
+// Your code below here...
+
+//step 1
+iceCreamFlavours.push("root beer");
+console.log(iceCreamFlavours);
+
+//step 2
+console.log(iceCreamFlavours[0]);
+
+//step 3
+console.log(iceCreamFlavours[5]);
+
+//step 4
+console.log(iceCreamFlavours.length);
