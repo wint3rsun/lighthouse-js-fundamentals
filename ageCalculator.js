c6f7f0bd77937032f0c1ab8e27b704bdaa99b548
Abigail Addae <abigail2addae@gmail.com>
2021-11-13 03:41:51 +0000

    practice writing function to calculate age

---
+++ ageCalculator.js
@@ -0,0 +1,7 @@
+function ageCalculator(name, birthYear, currentYear) {  
+  return name + " is " + (currentYear - birthYear) + " years old.";
+}
+
+console.log(ageCalculator("Suzie", 1983, 2015));
+console.log(ageCalculator("Miranda", 1983, 2015));
+console.log(ageCalculator("Ferdinand", 1988, 2015));
