d9f33499595942874344a952422d1c98e45972a5
Abigail Addae <abigail2addae@gmail.com>
2021-11-13 03:03:30 +0000

    practice making isEven function

---
+++ even.js
@@ -0,0 +1,12 @@
+const isEven = function (num) {
+  return num % 2 === 0; 
+}
+
+// const tenIsEven = isEven(10);
+// const elevelIsEven = isEven(11);
+
+// console.log(tenIsEven);
+// console.log(elevelIsEven);
+
+console.log(isEven(10));
+console.log(isEven(11));
