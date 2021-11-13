a5eb9b5324c47c3e38338f3240d0c122dded4b38
Abigail Addae <abigail2addae@gmail.com>
2021-11-13 21:29:26 +0000

    added solution for driving mayor daisy assignment

---
+++ objects-driving-mayor-daisy.js
@@ -0,0 +1,26 @@
+function carPassing(cars, speed) {
+  let updatedCars = cars;
+
+  updatedCars.push({time: Date.now(), speed: speed});
+
+  return updatedCars;
+}
+
+const cars = [
+  {
+    time: 1568329654807,
+    speed: 40,
+  },
+  {
+    time: 1568329821632,
+    speed: 42,
+  },
+  {
+    time: 1568331115463,
+    speed: 35
+  }
+]
+
+const speed = 38
+
+console.log(carPassing(cars, speed));
